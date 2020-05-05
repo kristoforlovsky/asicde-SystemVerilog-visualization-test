560f621f8db4c10e07d232fb683c51ebc88c6f57
kristoforlovsky <kristoforlovsky@gmail.com>
2020-05-05 13:25:11 +0200

    mminor changes

--- mod4Bports.sv
+++ mod4Bports.sv
@@ -1,13 +1,12 @@
 module TestModule (
+  input logic       	I1,
+  input logic       	I2,
+  output logic       	O1,
+  output logic       	O2
 );
 
 
-Mod1 1 (
-  .I1    (),
-  .I2    (),
-  .O1    (),
-  .O2    ()
-);
-
+assign O1  = I1_to_O1;
+assign O2  = I2_to_O2;
 
 endmodule: TestModule
